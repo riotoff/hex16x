ce0592f7f66c983a156e8601d2f340b0d1ddbe5b
Arseniy Rayotov <daxkod3243@gmail.com>
2025-04-08 17:25:14 +0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,1 +1,6 @@
-# Soon
+nasm -f bin src/bootloader.asm -o build/bootloader.bin
+nasm -f bin src/kernel.asm -o build/kernel.bin
+dd if=/dev/zero of=build/hex16x.img bs=512 count=2880
+dd if=build/bootloader.bin of=build/hex16x.img conv=notrunc
+dd if=build/kernel.bin of=build/hex16x.img bs=512 seek=1 conv=notrunc
+qemu-system-x86_64 -drive file=build/hex16x.img,format=raw,if=floppy
