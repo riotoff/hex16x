1458d87959bd4324661f3f01535dd42990ab3a02
Arseniy Rayotov <daxkod3243@gmail.com>
2025-04-11 12:06:37 +0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,5 +1,5 @@
 nasm -f bin src/bootloader/bootloader.asm -o build/bootloader.bin
-nasm -f bin src/kernel/kernel.asm -o build/kernel.bin
+nasm -f bin src/kernel/main.asm -o build/kernel.bin
 dd if=/dev/zero of=build/hex16x.img bs=512 count=2880
 dd if=build/bootloader.bin of=build/hex16x.img conv=notrunc
 dd if=build/kernel.bin of=build/hex16x.img bs=512 seek=1 conv=notrunc
